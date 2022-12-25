a0aae44c03d94220e3fe0a6c1a2b7a0cc34b5722
anastasijasv <sviridova.anastasija@gmail.com>
2022-12-25 22:12:33 +0200

    modified lesson 1

---
+++ 01. Basic JavaScript/index_02.js
@@ -0,0 +1,98 @@
+// Task 11. Add Two Numbers with JavaScript
+
+const sum = 10 + 10;
+
+
+// Task 12. Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
+
+
+// Task 13. Multiply Two Numbers with JavaScript
+
+const product1 = 8 * 10;
+
+
+// Task 14. Divide One Number by Another with JavaScript
+
+const quotient1 = 66 / 33;
+
+
+// Task 15. Increment a Number with JavaScript
+
+let myVar1 = 87;
+myVar1++;
+
+
+// Task 16. Decrement a Number with JavaScript
+
+let myVar2 = 11;
+myVar2--;
+
+
+// Task 17. Create Decimal Numbers with JavaScript
+
+const myDecimal = 5.7;
+
+
+// Task 18. Multiply Two Decimals with JavaScript
+
+const product2 = 2.0 * 2.5;
+
+
+// Task 19. Divide One Decimal by Another with JavaScript
+
+const quotient2 = 4.4 / 2.0
+
+
+// Task 20. Finding a Remainder in JavaScript
+
+/* In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2:
+17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is Even) */
+
+const remainder = 11 % 3;
+
+
+// Task 21. Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+
+// Task 22. Compound Assignment With Augmented Subtraction
+
+let d = 11;
+let e = 9;
+let f = 3;
+
+d -= 6;
+e -= 15;
+f -= 1;
+
+
+// Task 23. Compound Assignment With Augmented Multiplication
+
+let g = 5;
+let h = 12;
+let i = 4.6;
+
+g *= 5;
+h *= 3;
+i *= 10;
+
+
+// Task 24. Compound Assignment With Augmented Division
+
+let j = 48;
+let k = 108;
+let l = 33;
+
+j /= 12;
+k /= 4;
+l /= 11;
---
+++ 01. Basic JavaScript/index_03.js
@@ -0,0 +1,2 @@
+// Task 25. Escaping Literal Quotes in Strings
+
