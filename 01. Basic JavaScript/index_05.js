13be816eecdb281c5c676d5f456d551656295431
anastasijasv <sviridova.anastasija@gmail.com>
2023-01-12 13:49:35 +0200

    modified lesson 1

--- 01. Basic JavaScript/index_05.js
+++ 01. Basic JavaScript/index_05.js
@@ -1,1 +1,18 @@
 // Task 49. Write Reusable JavaScript with Functions
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
+
+// Task 50. Passing Values to Functions with Arguments
+
+function functionWithArgs(first, second) {
+  console.log(first + second);
+}
+
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
+
+// Task 51. Return a Value from a Function with Return
